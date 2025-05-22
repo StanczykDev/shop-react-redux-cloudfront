91c8747eb02cbb676d20f4e79a089addca627b6a
Nikita Stankevichius <stanczyk.dev@protonmail.com>
2025-05-22 21:16:18 +0200

    Task 8 complete.

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://3c32wuaowa.execute-api.eu-north-1.amazonaws.com/prod",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/prod",
+  order: "https://brqu6mbt39.execute-api.eu-north-1.amazonaws.com/prod/api",
   import: "https://mv1fsspphi.execute-api.eu-north-1.amazonaws.com/prod",
   bff: "https://3c32wuaowa.execute-api.eu-north-1.amazonaws.com/prod",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/prod",
+  cart: "https://brqu6mbt39.execute-api.eu-north-1.amazonaws.com/prod/api",
 };
 
 export default API_PATHS;
